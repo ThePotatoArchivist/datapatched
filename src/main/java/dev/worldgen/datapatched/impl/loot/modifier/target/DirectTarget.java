8b157aedb5a4e6beab58d66872b81d444a35f826
ThePotatoArchivist <josiahfu@gmail.com>
2026-09-12 19:56:53 -0700

    Inline compactListCodec for portability

--- src/main/java/dev/worldgen/datapatched/impl/loot/modifier/target/DirectTarget.java
+++ src/main/java/dev/worldgen/datapatched/impl/loot/modifier/target/DirectTarget.java
@@ -1,15 +1,18 @@
 package dev.worldgen.datapatched.impl.loot.modifier.target;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import net.minecraft.resources.Identifier;
-import net.minecraft.util.ExtraCodecs;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public record DirectTarget(List<Identifier> ids) implements Predicate<Identifier> {
-    public static final Codec<DirectTarget> CODEC = ExtraCodecs.compactListCodec(Identifier.CODEC)
-        .xmap(DirectTarget::new, DirectTarget::ids);
+    public static final Codec<DirectTarget> CODEC = Codec.either(Identifier.CODEC, Identifier.CODEC.listOf()).xmap(
+        either -> either.map(List::of, Function.identity()),
+        ids -> ids.size() == 1 ? Either.left(ids.getFirst()) : Either.right(ids)
+    ).xmap(DirectTarget::new, DirectTarget::ids);
 
     @Override
     public boolean test(Identifier identifier) {
